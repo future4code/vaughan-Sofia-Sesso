d4b0aa671bb124de87685956d88b9e129c07752f
SofiaPittaSesso <sofit_sops@hotmail.com>
2021-12-12 21:45:32 -0300

    Tentei fazer o desafio

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,33 +11,88 @@
  * 
  */
 
-// ainda estou trabalhando neste desafio
+const cartasUsuario = []
+const cartasComputador = []
 
-// console.log("Boas vindas ao jogo de Blackjack!")
-
-// const cartaUsuario1 = comprarCarta()
-// const cartaUsuario2 = comprarCarta()
-// const cartaComputador1 = comprarCarta()
-// const cartaComputador2 = comprarCarta()
-
-// while (confirm("Quer iniciar uma nova rodada?")) {
-//    if ((cartaUsuario1.texto && cartaUsuario2.texto).includes("A")) {
-//       console.log("Um dos jogadores recebeu dois ases. Reinicia o jogo")
-//    } else if (confirm(`Suas cartas são ${cartaUsuario1.texto} ${cartaUsuario2.texto}. A carta revelada do computador é ${cartaComputador1.texto}\nDeseja comprar mais?`)) {
-//       const cartaUsuario3 = comprarCarta()
-//       const cartaComputador3 = comprarCarta()
-
-//       if (cartaComputador1.valor + cartaComputador2.valor + cartaComputador3.valor < 21) {
-//          const cartaComputador4 = comprarCarta()
-//          const soma = cartaComputador1.valor + cartaComputador2.valor + cartaComputador3.valor + cartaComputador4.valor
-//          return soma
-//       }
-
-//       if (cartaUsuario1.valor + cartaUsuario2.valor + cartaUsuario3.valor < 21) {
-//              confirm(`Suas cartas são ${cartaUsuario1.texto} ${cartaUsuario2.texto} ${cartaUsuario3}. A carta revelada do computador é ${cartaComputador1.texto}\nDeseja comprar mais?`)
-//       } else if (cartaUsuario1 + cartaUsuario2 + cartaUsuario3 >
-//          cartaComputador1 + cartaComputador2 + cartaComputador3 && 
-//          cartaUsuario1 + cartaUsuario2 + cartaUsuario3 <= 21) {}
+function comprarCartasParaOsDoisJogadores () {
+   cartasUsuario.push(comprarCarta())
+   cartasComputador.push(comprarCarta())
    
-//    }
-// }
+}
+
+if (confirm("Bem-vindo ao jogo de Blackjack!\nQuer iniciar uma rodada?")) {
+   
+   comprarCartasParaOsDoisJogadores()
+   comprarCartasParaOsDoisJogadores()
+
+   const textoCartasUsuario = cartasUsuario.map((carta) => {
+      return carta.texto
+   })
+
+   const valorCartasUsuario = cartasUsuario.map((carta) => {
+      return carta.valor
+   })
+
+   const tem2AsesUsuario = cartasUsuario.filter((carta) => {
+      return carta.texto.includes("A")
+   })
+      
+   const somaValoresUsuario = valorCartasUsuario.reduce((total, elemento) => {
+      return total += elemento
+   })
+
+   const textoCartasComputador = cartasComputador.map((carta) => {
+      return carta.texto
+   })
+
+   const tem2AsesComputador = cartasComputador.filter((carta) => {
+      return carta.texto.includes("A")
+   })
+
+   const valorCartasComputador = cartasComputador.map((carta) => {
+      return carta.valor
+   })
+
+   const somaValoresComputador = valorCartasComputador.reduce((total, elemento) => {
+      return total += elemento
+   })
+     
+   if (confirm(`Suas cartas são ${textoCartasUsuario}, deseja comprar?`)) {
+      comprarCartasParaOsDoisJogadores()
+   }
+  
+   if (tem2AsesUsuario.length === 2 || tem2AsesComputador.length === 2) {
+      alert("Um dos jogadores recebeu dois ases. Acabou o jogo")
+   } else {
+      while (somaValoresUsuario < 21) { 
+         if (confirm(`Suas cartas são ${textoCartasUsuario}. A carta revelada do computador é ${textoCartasComputador[0]}
+      Deseja comprar mais umas carta?`)) {
+
+            comprarCartasParaOsDoisJogadores()
+            
+            console.log(somaValoresUsuario)
+
+         } else { 
+            if (somaValoresUsuario > somaValoresComputador && somaValoresUsuario <= 21) {
+               alert(`Suas cartas são ${textoCartasUsuario}. Sua pontuação é ${somaValoresUsuario}.\nAs cartas do computador são ${textoCartasComputador}. A pontuação do computador é ${somaValoresComputador}\nVocê ganhou!`)
+
+            } else if (somaValoresComputador > somaValoresUsuario && somaValoresComputador <= 21) {
+               alert(`Suas cartas são ${textoCartasUsuario}. Sua pontuação é ${somaValoresUsuario}.\nAs cartas do computador são ${textoCartasComputador}. A pontuação do computador é ${somaValoresComputador}\nO computador ganhou!`)
+
+            } else if (somaValoresUsuario > 21 && somaValoresComputador <= 21) {
+               alert(`Suas cartas são ${textoCartasUsuario}. Sua pontuação é ${somaValoresUsuario}.\nAs cartas do computador são ${textoCartasComputador}. A pontuação do computador é ${somaValoresComputador}\nO computador ganhou!`)
+
+            } else if (somaValoresComputador > 21 && somaValoresUsuario <= 21) {
+               alert(`Suas cartas são ${textoCartasUsuario}. Sua pontuação é ${somaValoresUsuario}.\nAs cartas do computador são ${textoCartasComputador}. A pontuação do computador é ${somaValoresComputador}\nVocê ganhou!`)
+
+            } else if (somaValoresUsuario === somaValoresComputador) {
+               alert(`Suas cartas são ${textoCartasUsuario}. Sua pontuação é ${somaValoresUsuario}.\nAs cartas do computador são ${textoCartasComputador}. A pontuação do computador é ${somaValoresComputador}\nEmpate!`)
+            }
+         }
+          
+      }
+   }
+      
+} else {
+   alert("O jogo acabou")
+}
--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -11,25 +11,25 @@
  * 
  */
 
-console.log("Boas vindas ao jogo de Blackjack!")
+// console.log("Boas vindas ao jogo de Blackjack!")
 
-const cartaUsuario1 = comprarCarta()
-const cartaUsuario2 = comprarCarta()
-const cartaComputador1 = comprarCarta()
-const cartaComputador2 = comprarCarta()
+// const cartaUsuario1 = comprarCarta()
+// const cartaUsuario2 = comprarCarta()
+// const cartaComputador1 = comprarCarta()
+// const cartaComputador2 = comprarCarta()
 
-if (confirm("Quer iniciar uma nova rodada?")) {
-   console.log(`Usuário - cartas: ${cartaUsuario1.texto} ${cartaUsuario2.texto} - pontuação: ${cartaUsuario1.valor + cartaUsuario2.valor}`)
-   console.log(`Computador - cartas: ${cartaComputador1.texto} ${cartaComputador2.texto} - pontuação: ${cartaComputador1.valor + cartaComputador2.valor}`)
+// if (confirm("Quer iniciar uma nova rodada?")) {
+//    console.log(`Usuário - cartas: ${cartaUsuario1.texto} ${cartaUsuario2.texto} - pontuação: ${cartaUsuario1.valor + cartaUsuario2.valor}`)
+//    console.log(`Computador - cartas: ${cartaComputador1.texto} ${cartaComputador2.texto} - pontuação: ${cartaComputador1.valor + cartaComputador2.valor}`)
 
-   if (cartaUsuario1.valor + cartaUsuario2.valor > cartaComputador1.valor + cartaComputador2.valor) {
-      console.log("O usuário ganhou!")
-   } else if (cartaComputador1.valor + cartaComputador2.valor > cartaUsuario1.valor + cartaUsuario2.valor) {
-      console.log("O computador ganhou!")
-   } else if (cartaUsuario1.valor + cartaUsuario2.valor === cartaComputador1 + cartaComputador2) {
-      console.log("Empate!")
-   }
+//    if (cartaUsuario1.valor + cartaUsuario2.valor > cartaComputador1.valor + cartaComputador2.valor) {
+//       console.log("O usuário ganhou!")
+//    } else if (cartaComputador1.valor + cartaComputador2.valor > cartaUsuario1.valor + cartaUsuario2.valor) {
+//       console.log("O computador ganhou!")
+//    } else if (cartaUsuario1.valor + cartaUsuario2.valor === cartaComputador1 + cartaComputador2) {
+//       console.log("Empate!")
+//    }
 
-} else {
-   console.log("O jogo acabou")
-}
+// } else {
+//    console.log("O jogo acabou")
+// }
