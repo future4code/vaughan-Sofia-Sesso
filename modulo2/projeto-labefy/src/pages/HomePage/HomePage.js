626168f4cec9d96366db004df334cde326a02bd0
Sofia Pitta Sesso <sofit_sops@hotmail.com>
2022-01-29 18:06:09 -0300

    Icones adicionados

--- modulo2/projeto-labefy/src/pages/HomePage/HomePage.js
+++ modulo2/projeto-labefy/src/pages/HomePage/HomePage.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { MainContainer, Button } from "./styled"
+import homePageIcon from "../../images/homePageIcon.png"
 
 export default function HomePage(props) {
     return <MainContainer>
         <h1>Playtime!</h1>
         <img 
-            src="https://cdn-icons.flaticon.com/png/512/2402/premium/2402461.png?token=exp=1643485143~hmac=4553c20f14113feb259f3397035202ee"
+            src={homePageIcon}
             alt="Icone Musica"
         ></img>
         <Button onClick={props.switchToPlaylists}><h3>Acesse suas Playlists</h3></Button>
