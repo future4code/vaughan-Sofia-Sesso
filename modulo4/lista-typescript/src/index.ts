09998735efe552ccf4f341b1fc8409c8961bd62b
Sofia Pitta Sesso <sofit_sops@hotmail.com>
2022-03-27 14:19:04 -0300

    exercicio 11

--- modulo4/lista-typescript/src/index.ts
+++ modulo4/lista-typescript/src/index.ts
@@ -8,14 +8,16 @@ import { produtos, ordenaPorQuantidade } from './Exercícios/ex-7'
 import { precisaRenovarCarteira } from './Exercícios/ex-8'
 import { retornaQuantidadeDeAnagramas } from './Exercícios/ex-9'
 import { verificaCPF } from './Exercícios/ex-10'
+import { converteParaNumerosRomanos } from './Exercícios/ex-11'
 
-// console.log(retornaDiaDoNascimento("Sofia", "02/03/1995"))
-// imprimeTipo(true)
-// console.table(retornaInfoDoFilme("John Wick", 2014, GENERO.ACAO, 100))
-// console.table(colaboradoresMarketingPresencial(colaboradores))
-// console.table(adminEmails(users))
-// console.table(acharClientesComSaldoNegativo(clientes))
-// console.table(ordenaPorQuantidade(produtos))
-// console.log(precisaRenovarCarteira("02/03/1995", "10/10/2010"))
-// console.log(retornaQuantidadeDeAnagramas("gatinho"))
+console.log(retornaDiaDoNascimento("Sofia", "02/03/1995"))
+imprimeTipo(true)
+console.table(retornaInfoDoFilme("John Wick", 2014, GENERO.ACAO, 100))
+console.table(colaboradoresMarketingPresencial(colaboradores))
+console.table(adminEmails(users))
+console.table(acharClientesComSaldoNegativo(clientes))
+console.table(ordenaPorQuantidade(produtos))
+console.log(precisaRenovarCarteira("02/03/1995", "10/10/2010"))
+console.log(retornaQuantidadeDeAnagramas("gatinho"))
 console.log(verificaCPF("145.382.206-89"))
+console.log(converteParaNumerosRomanos(1990))
