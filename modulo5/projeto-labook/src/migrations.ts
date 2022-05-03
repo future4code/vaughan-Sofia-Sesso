d21c354cf179bd569f055dc5818f94a03d4608de
Sofia Pitta Sesso <sofit_sops@hotmail.com>
2022-05-03 15:06:56 -0300

    Setup

---
+++ modulo5/projeto-labook/src/migrations.ts
@@ -0,0 +1,30 @@
+import { BaseDatabase } from "./data/BaseDatabase"
+
+class Migrations extends BaseDatabase {
+    createTables = async (): Promise<void> => {
+        this.connection
+            .raw(`
+        CREATE TABLE IF NOT EXISTS labook_users(
+            id VARCHAR(255) PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL
+      );
+
+        CREATE TABLE IF NOT EXISTS labook_posts(
+            id VARCHAR(255) PRIMARY KEY,
+            photo VARCHAR(255) NOT NULL,
+            description VARCHAR(255) NOT NULL,
+            type ENUM("normal","event") DEFAULT "normal",
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            author_id VARCHAR(255),
+            FOREIGN KEY (author_id) REFERENCES labook_users (id)
+      )
+    `)
+            .then(console.log)
+            .catch(console.log)
+    }
+}
+
+const migrations: Migrations = new Migrations()
+migrations.createTables()
